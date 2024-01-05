b7c45b26955b152ade3dfcf4197150c504830c52
Kristian Kjerstad <kristian.kjerstad@brilliant.no>
2024-01-05 14:51:19 +0100

    chore: add vercel backend config

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,7 @@
+from os import getenv
 from typing import Union
 
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -13,3 +15,9 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    port = int(getenv("PORT", 5000))
+    host = getenv("HOST", "0.0.0.0")  # nosec
+    uvicorn.run("main:app", host=host, port=port, reload=True)
