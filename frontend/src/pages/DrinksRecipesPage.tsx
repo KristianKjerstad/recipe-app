a55ae3f31d0f5194174f743b83e370e6a07f024f
Kristian Kjerstad <kristian.kjerstad@brilliant.no>
2024-01-09 16:07:32 +0100

    wip: drink ingredient multiselect

--- frontend/src/pages/DrinksRecipesPage.tsx
+++ frontend/src/pages/DrinksRecipesPage.tsx
@@ -1,3 +1,58 @@
+import { useState } from 'react'
+import makeAnimated from 'react-select/animated'
+const animatedComponents = makeAnimated()
+
+import Select, { MultiValue } from 'react-select'
+
+type Options = {
+    label: string
+    options: { label: string; value: string }[]
+}
+
+const sampleOptions: Options[] = [
+    {
+        label: 'Finland',
+        options: [
+            {
+                label: 'Great Hotel',
+                value: 'Great Hotel',
+            },
+        ],
+    },
+    {
+        label: 'Sweden',
+        options: [{ label: 'Stockholm', value: 'Stockholm' }],
+    },
+]
+
 export const DrinksRecipesPage = () => {
-    return <div>drinks</div>
+    const [selectedIngredients, setSelectedIngredients] = useState<
+        MultiValue<Options>
+    >([])
+
+    const handleSelectedIngredientChange = (value: MultiValue<Options>) => {
+        setSelectedIngredients(value)
+        console.log(value)
+    }
+
+    return (
+        <div>
+            <h3>Drinks</h3>
+            <div className="text-left pt-6">
+                <p className="pb-2">Select your ingredients</p>
+                <Select
+                    className="my-react-select-container"
+                    classNamePrefix="my-react-select"
+                    value={selectedIngredients}
+                    options={sampleOptions}
+                    onChange={(value) => {
+                        handleSelectedIngredientChange(value)
+                    }}
+                    isMulti
+                    closeMenuOnSelect={false}
+                    hideSelectedOptions={true}
+                />
+            </div>
+        </div>
+    )
 }
