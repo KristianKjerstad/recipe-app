b4a15bb41c8e356c05e15e5d71c0475d05dac312
Kristian Kjerstad <kristian.kjerstad@brilliant.no>
2024-02-08 14:25:02 +0100

    fix: tests

--- frontend/src/tests/filter.test.ts
+++ frontend/src/tests/filter.test.ts
@@ -3,12 +3,29 @@ import { Recipe } from '../api/generated'
 import { filterRecipes } from '../utils/filtering'
 
 test('filter recipes by ingredient', () => {
+    const ingredient_1 = {
+        ingredient_uuid: '4f556d17-97f4-47ad-828c-088c7041a6b1',
+        ingredient_quantity: '1 ml',
+    }
+    const ingredient_2 = {
+        ingredient_uuid: '4f556d17-97f4-47ad-828c-088c7041a6b2',
+        ingredient_quantity: '2 ml',
+    }
+    const ingredient_3 = {
+        ingredient_uuid: '4f556d17-97f4-47ad-828c-088c7041a6b3',
+        ingredient_quantity: '3 ml',
+    }
+    const ingredient_4 = {
+        ingredient_uuid: '4f556d17-97f4-47ad-828c-088c7041a6b4',
+        ingredient_quantity: '4 ml',
+    }
+
     const vodkaRedbullRecipe: Recipe = {
         id: '81ff95d9-c553-4fc9-87cd-e743f8457474',
         type: 'cocktail',
         name: 'Vodka redbull',
         category: 'cocktail',
-        ingredient_ids: ['1', '2'],
+        ingredients: [ingredient_1, ingredient_2],
         recipe_steps: ['Mix ingredients', 'drink!'],
     }
     const margaritaRecipe: Recipe = {
@@ -16,7 +33,7 @@ test('filter recipes by ingredient', () => {
         type: 'cocktail',
         name: 'Margarita',
         category: 'cocktail',
-        ingredient_ids: ['3', '4', '1'],
+        ingredients: [ingredient_3, ingredient_4, ingredient_1],
         recipe_steps: ['Mix ingredients', 'drink!'],
     }
     const recipes: Recipe[] = [vodkaRedbullRecipe, margaritaRecipe]
@@ -30,34 +47,59 @@ test('filter recipes by ingredient', () => {
     expect(
         filterRecipes({
             allRecipes: recipes,
-            selectedIngredientIds: ['1', '2'],
+            selectedIngredientIds: [
+                ingredient_1.ingredient_uuid,
+                ingredient_2.ingredient_uuid,
+            ],
         })
     ).toEqual([vodkaRedbullRecipe])
     expect(
         filterRecipes({
             allRecipes: recipes,
-            selectedIngredientIds: ['1', '2', '3'],
+            selectedIngredientIds: [
+                ingredient_1.ingredient_uuid,
+                ingredient_2.ingredient_uuid,
+                ingredient_3.ingredient_uuid,
+            ],
         })
     ).toEqual([vodkaRedbullRecipe])
     expect(
-        filterRecipes({ allRecipes: recipes, selectedIngredientIds: ['2'] })
+        filterRecipes({
+            allRecipes: recipes,
+            selectedIngredientIds: [ingredient_2.ingredient_uuid],
+        })
     ).toEqual([])
     expect(
         filterRecipes({
             allRecipes: recipes,
-            selectedIngredientIds: ['3', '4', '1'],
+            selectedIngredientIds: [
+                ingredient_3.ingredient_uuid,
+                ingredient_4.ingredient_uuid,
+                ingredient_1.ingredient_uuid,
+            ],
         })
     ).toEqual([margaritaRecipe])
     expect(
         filterRecipes({
             allRecipes: recipes,
-            selectedIngredientIds: ['3', '4', '1', '87', '99'],
+            selectedIngredientIds: [
+                ingredient_3.ingredient_uuid,
+                ingredient_4.ingredient_uuid,
+                ingredient_1.ingredient_uuid,
+                '87',
+                '99',
+            ],
         })
     ).toEqual([margaritaRecipe])
     expect(
         filterRecipes({
             allRecipes: recipes,
-            selectedIngredientIds: ['1', '2', '3', '4'],
+            selectedIngredientIds: [
+                ingredient_1.ingredient_uuid,
+                ingredient_2.ingredient_uuid,
+                ingredient_3.ingredient_uuid,
+                ingredient_4.ingredient_uuid,
+            ],
         })
     ).toEqual(recipes)
 })
