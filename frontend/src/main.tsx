a65367d0be303ca0336b3f484aaedf286c195e22
Kristian Kjerstad <kristian.kjerstad@brilliant.no>
2024-08-15 10:50:16 +0200

    add vercel speed insights

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import '@mantine/core/styles.css'
 import { ThemeProvider } from '@mui/material/styles'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ErrorBoundary } from 'react-error-boundary'
@@ -29,6 +30,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
                                 </div>
                                 <Footer />
                                 <Analytics />
+                                <SpeedInsights />
                             </div>
                         </QueryClientProvider>
                     </MantineProvider>
