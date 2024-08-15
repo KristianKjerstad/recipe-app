6b18977977e800afe7925c4a18add9f7f365c5b3
Kristian Kjerstad <kristian.kjerstad@brilliant.no>
2024-08-15 10:48:50 +0200

    add vercel analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css'
 import { ThemeProvider } from '@mui/material/styles'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Analytics } from '@vercel/analytics/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ErrorBoundary } from 'react-error-boundary'
@@ -27,6 +28,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
                                     <App />
                                 </div>
                                 <Footer />
+                                <Analytics />
                             </div>
                         </QueryClientProvider>
                     </MantineProvider>
